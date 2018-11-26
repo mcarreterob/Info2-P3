782c3cbd3065682543f96a0a261bd9ff14f84e59
Mario Carretero Berrocal <mariocb@alpha06.aulas.gsyc.urjc.es>
2018-11-26 20:11:40 +0100

    lunes26

--- chat_client_2.adb
+++ chat_client_2.adb
@@ -4,24 +4,24 @@ with Ada.Text_IO;
 with Ada.Exceptions;
 with Chat_Messages;
 with Ada.Command_Line;
-with Client_Collections;
+with Client_Handler;
 
 procedure Chat_Client_2 is
+   package CH renames Client_Handler;
    package LLU renames Lower_Layer_UDP;
    package ASU renames Ada.Strings.Unbounded;
 	package ATIO renames Ada.Text_IO;
 	package ACL renames Ada.Command_Line;
 	package CM renames Chat_Messages;
-	package CC renames Client_Collections;
 	use type CM.Message_Type;
 	use type ASU.Unbounded_String;
 
    Server_EP: LLU.End_Point_Type;
-   Client_EP_Receive: LLU.End_Point_Type; --Para recibir los Welcome
-   Client_EP_Handler: LLU.End_Point_Type; --Para recibir mensajes del server
+   Client_EP_Receive: LLU.End_Point_Type;
+   Client_EP_Handler: LLU.End_Point_Type;
    Buffer: aliased LLU.Buffer_Type(1024);
-   Request: ASU.Unbounded_String;
-   Reply: ASU.Unbounded_String;
+   -- Request: ASU.Unbounded_String;
+   Comentario: ASU.Unbounded_String;
    Expired : Boolean;
 	Mess: CM.Message_Type;
 	Maquina: ASU.Unbounded_String := ASU.To_Unbounded_String(ACL.Argument(1));
@@ -29,12 +29,12 @@ procedure Chat_Client_2 is
 	IP: String := LLU.To_IP(ASU.To_String(Maquina));
 	Puerto: Integer;
 	Finish: Boolean := False;
-	Comprobacion: ASU.Unbounded_String;
    Acogido: Boolean;
 
    Usage_Error: exception;
    Name_Error: exception;
    Server_Error: exception;
+   Welcomed_Error: exception;
 
 begin
 	--Maquina := ASU.To_Unbounded_String(ACL.Argument(1));
@@ -46,98 +46,72 @@ begin
 		raise Usage_Error;
 	end if;
 
-   Server_EP := LLU.Build(IP, Puerto);
-   LLU.Bind_Any(Client_EP_Receive);
-
-	-- ATIO.Put_Line("Mini-Chat v2.0: Welcome " & ASU.To_String(Nick));
-	-- ATIO.Put_Line(">>");
-
 	if Nick = "server" then
 		raise Name_Error;
 	else
+      -- EP del Server
+      Server_EP := LLU.Build(IP, Puerto);
+      -- Donde recibire el Welcome
+      LLU.Bind_Any(Client_EP_Receive);
+      -- Donde recibire mensajes del Server
+      LLU.Bind_Any(Client_EP_Handler, CH.Handler_Client'Access);
    	LLU.Reset(Buffer);
 		Mess := CM.Init;
-		CM.Message_Type'Output(buffer'Access, Mess);
+		CM.Message_Type'Output(Buffer'Access, Mess);
 		LLU.End_Point_Type'Output(Buffer'Access, Client_EP_Receive);
 		LLU.End_Point_Type'Output(Buffer'Access, Client_EP_Handler);
    	ASU.Unbounded_String'Output(Buffer'Access, Nick);
    	LLU.Send(Server_EP, Buffer'Access);
    	LLU.Reset(Buffer);
 		LLU.Receive(Client_EP_Receive, Buffer'Access, 10.0, Expired);
-      Mess := Mesage_Type'Input(Buffer'Access);
+      Mess := CM.Message_Type'Input(Buffer'Access);
       Acogido := Boolean'Input(Buffer'Access);
       if Expired then
          raise Server_Error;
       end if;
-      if Acogido
+      if Acogido then
+         -- MODO ESCRITOR
+         ATIO.Put_Line("Mini-Chat v2.0: Welcome " & ASU.To_String(Nick));
+         while not Finish loop
+            LLU.Reset(Buffer);
+            ATIO.Put(">>");
+            Mess := CM.Writer;
+            CM.Message_Type'Output(Buffer'Access, Mess);
+            LLU.End_Point_Type'Output(Buffer'Access, Client_EP_Handler);
+            ASU.Unbounded_String'Output(Buffer'Access, Nick);
+            Comentario := ASU.To_Unbounded_String(Ada.Text_IO.Get_Line);
+            ASU.Unbounded_String'Output(Buffer'Access, Comentario);
+         	LLU.Send(Server_EP, Buffer'Access);
+            if Comentario = ".quit" then
+               Finish := True;
+               Mess := CM.Logout;
+               CM.Message_Type'Output(Buffer'Access, Mess);
+               LLU.End_Point_Type'Output(Buffer'Access, Client_EP_Handler);
+               ASU.Unbounded_String'Output(Buffer'Access, Nick);
+            	LLU.Send(Server_EP, Buffer'Access);
+               LLU.Finalize;
+            end if;
+         end loop;
+      else
+         raise Welcomed_Error;
+      end if;
 	end if;
--------------------------------------------------------------------------
------------------------------------P2------------------------------------
--------------------------------------------------------------------------
-	-- if Nick = "reader" then
-	-- 	--MODO LECTOR
-   -- 	LLU.Reset(Buffer);
-	-- 	Mess := CM.Init;
-	-- 	CM.Message_Type'Output(buffer'Access, Mess);
-	-- 	LLU.End_Point_Type'Output(Buffer'Access, Client_EP_Receive);
-   -- 	ASU.Unbounded_String'Output(Buffer'Access, Nick);
-   --
-   -- 	LLU.Send(Server_EP, Buffer'Access);
-   --
-   -- 	LLU.Reset(Buffer);
-   --
-	-- 	loop
-   -- 		LLU.Receive(Client_EP_Receive, Buffer'Access, 1000.0, Expired);
-	-- 		if Expired then
-	-- 			Ada.Text_IO.Put_Line ("Plazo expirado");
-	-- 		else
-	-- 			Mess := CM.Message_Type'Input(Buffer'Access);
-	-- 			Nick := ASU.Unbounded_String'Input(Buffer'Access);
-	-- 			Reply := ASU.Unbounded_String'Input(Buffer'Access);
-	-- 			ATIO.Put_Line(ASU.To_String(Nick) & ": " & ASU.To_String(Reply));
-	-- 			LLU.Reset(Buffer);
-	-- 		end if;
-	-- 	end loop;
-	-- else
-	-- 	--MODO ESCRITOR
-   -- 	LLU.Reset(Buffer);
-	-- 	Mess := CM.Init;
-	-- 	CM.Message_Type'Output(buffer'Access, Mess);
-	-- 	LLU.End_Point_Type'Output(Buffer'Access, Client_EP_Receive);
-   -- 	ASU.Unbounded_String'Output(Buffer'Access, Nick);
-   -- 	LLU.Send(Server_EP, Buffer'Access);
-   -- 	LLU.Reset(Buffer);
-   --
-	-- 	while not Finish loop
-	-- 		--MODO ESCRITOR
-	-- 		LLU.Reset(Buffer);
-	-- 		Mess := CM.Writer;
-	-- 		CM.Message_Type'Output(buffer'Access, Mess);
-	-- 		ATIO.Put(">> ");
-	-- 		Request := ASU.To_Unbounded_String(Ada.Text_IO.Get_Line);
-	-- 		if Request = ".quit" then
-	-- 			Finish := True;
-	-- 		else
-	-- 			LLU.End_Point_Type'Output(Buffer'Access, Client_EP_Receive);
-	-- 			ASU.Unbounded_String'Output(Buffer'Access, Request);
-	-- 			LLU.Send(Server_EP, Buffer'Access);
-	-- 		end if;
-   -- 		LLU.Reset(Buffer);
-	-- 		ATIO.Put(">> ");
-	-- 	end loop;
-	-- end if;
 
    LLU.Finalize;
 
 exception
+   when Welcomed_Error =>
+      ATIO.Put_Line("Mini-Chat v2.0: IGNORED new user "
+                     & ASU.To_String(Nick) & ", nick already used");
+      LLU.Finalize;
 	when Usage_Error =>
-			ATIO.Put_Line("usage: ./chat_client_2 <host_name> <port> <nick>");
-			LLU.Finalize;
+		ATIO.Put_Line("usage: ./chat_client_2 <host_name> <port> <nick>");
+		LLU.Finalize;
 	when Name_Error =>
 		ATIO.Put_Line("Is not allowed to use 'server' as Nick");
 		LLU.Finalize;
    when Server_Error =>
-      ATIO.Put_Line("Unracheable server.")
+      ATIO.Put_Line("Unracheable server.");
       LLU.Finalize;
    when Ex:others =>
       ATIO.Put_Line ("Excepción imprevista: " &
--- server_handler.adb
+++ server_handler.adb
@@ -5,10 +5,12 @@ package body Server_Handler is
                      P_Buffer: access LLU.Buffer_Type) is
       Client_EP_Receive: LLU.End_Point_Type;
       Client_EP_Handler: LLU.End_Point_Type;
+      Datos_Cliente: Client_Data; -- TABLA DE SIMBOLOS
       Mess: CM.Message_Type;
       Nick: ASU.Unbounded_String;
       Comentario: ASU.Unbounded_String;
       Acogido: Boolean := False;
+      Hora_entrada: Ada.Calendar.Time;
    begin
       Mess:= CM.Message_Type'Input(P_Buffer);
       if Mess = CM.Init then
@@ -16,3 +18,13 @@ package body Server_Handler is
          Client_EP_Handler := LLU.End_Point_Type'Input(P_Buffer);
          Nick := ASU.Unbounded_String'Input(P_Buffer);
          LLU.Reset(P_Buffer.all);
+         ATIO.Put("INIT received from " & ASU.To_String(Nick) & ": ");
+         Active_Clients.Get(Map_Active, Client_Value, Success);
+         if not Success then
+            ATIO.Put_Line("ACCEPTED");
+            Comentario := ASU.To_Unbounded_String(ASU.To_String(Nick)
+                                                & " joins the chat.");
+            Hora_entrada := Ada.Calendar.Clock;
+            -- Meto los valores en la tabla
+            Datos_Cliente.Client_EP_Handler := Client_EP_Handler;
+            Datos_Cliente.Last_Connection := Hora_entrada;
