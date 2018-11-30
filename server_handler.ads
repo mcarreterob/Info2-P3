ee15fe21a3eb3a6ac85ac4bb2a7a99967ebf8aec
Mario Carretero Berrocal <mariocb@gamma13.aulas.gsyc.urjc.es>
2018-11-30 12:54:18 +0100

    viernes30

--- server_handler.ads
+++ server_handler.ads
@@ -4,6 +4,7 @@ with Ada.Strings.Unbounded;
 with Chat_Messages;
 with Ada.Command_Line;
 with Ada.Calendar;
+with Maps_G;
 
 package Server_Handler is
    package ATIO renames Ada.Text_IO;
@@ -16,30 +17,28 @@ package Server_Handler is
    use type Ada.Calendar.Time;
    use type LLU.End_Point_Type;
 
-   procedure Handler (From: in LLU.End_Point_Type;
-                     To: in LLU.End_Point_Type;
-                     P_Buffer: access LLU.Buffer_Type);
-
-   --TABLA DE SIMBOLOS
 	type Client_Data is record
 		Client_EP_Handler: LLU.End_Point_Type;
 		Last_Connection: Ada.Calendar.Time;
 	end record;
 
-   --CLIENTES INACTIVOS: Nick, Hora y Max:
    package Inactive_Clients is new Maps_G (Key_Type => ASU.Unbounded_String,
                                            Value_Type => Ada.Calendar.Time,
                                            Max_Size => 150,
                                            "=" => ASU."=");
 
-   --CLIENTES ACTIVOS: Nick, (EP_Handler, Hora) y Max:
    package Active_Clients is new Maps_G (Key_Type => ASU.Unbounded_String,
-                                         Value_Type => Client_Info,
-                                         Max_Size => Integer'Value(CL.Argument(2)),
+                                         Value_Type => Client_Data,
+                                         Max_Size => Integer'Value(ACL.Argument(2)),
                                          "=" => ASU."=");
 
 
 
+   --TABLA DE SIMBOLOS
    Map_Active: Active_Clients.Map;
    Map_Inactive: Inactive_Clients.Map;
+
+   procedure Handler (From: in LLU.End_Point_Type;
+                     To: in LLU.End_Point_Type;
+                     P_Buffer: access LLU.Buffer_Type);
 end Server_Handler;
